6f83dfef72fbb4e3a4149cf2769210f461a88d48
vikas <vikas@vikas-Vostro-2420.(none)>
2015-01-13 04:12:05 +0530

    i

--- src/com/proptiger1/CheckoutProcess_TestMatrix.java
+++ src/com/proptiger1/CheckoutProcess_TestMatrix.java
@@ -30,7 +30,7 @@ public class CheckoutProcess_TestMatrix {
 				+ "(KHTML, like Gecko) CriOS/30.0.1599.12 Mobile/11A501 Safari/8536.25");
 		WebDriver driver= new FirefoxDriver(profile);
 		PaymentGatewayTest.Pg_Test(driver);	
-		System.out.println("Checkout working fine in Chrome-iPhone");
+		System.out.println("1.Checkout working fine in Chrome-iPhone");
 		driver.quit();
 	}
 	
@@ -42,7 +42,7 @@ public class CheckoutProcess_TestMatrix {
 				+ "(KHTML, like Gecko) Chrome/18.0.1025.166 Safari/535.19");
 		WebDriver driver= new FirefoxDriver(profile);
 		PaymentGatewayTest.Pg_Test(driver);	
-		System.out.println("Checkout working fine in Chrome-Android-Tablet");
+		System.out.println("2.Checkout working fine in Chrome-Android-Tablet");
 		driver.quit();
 	}
 	
@@ -54,7 +54,7 @@ public class CheckoutProcess_TestMatrix {
 				+ "(KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19");
 		WebDriver driver= new FirefoxDriver(profile);
 		PaymentGatewayTest.Pg_Test(driver);	
-		System.out.println("Checkout working fine in Chrome-AndroidMobile");
+		System.out.println("3.Checkout working fine in Chrome-AndroidMobile");
 		driver.quit();
 	}
 	@Test(priority=3)
@@ -65,7 +65,7 @@ public class CheckoutProcess_TestMatrix {
 		profile.setPreference("general.useragent.override","Mozilla/5.0 (Android; Tablet; rv:14.0) Gecko/14.0 Firefox/14.0");
 		WebDriver driver= new FirefoxDriver(profile);
 		PaymentGatewayTest.Pg_Test(driver);	
-		System.out.println("Checkout working fine in FireFox-Android");
+		System.out.println("4.Checkout working fine in FireFox-Android");
 		driver.quit();
 	}
 	@Test(priority=4)
@@ -75,7 +75,7 @@ public class CheckoutProcess_TestMatrix {
 				+ "(KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4");
 		WebDriver driver= new FirefoxDriver(profile);
 		PaymentGatewayTest.Pg_Test(driver);	
-		System.out.println("Checkout working fine in iOS8-Safari");
+		System.out.println("5.Checkout working fine in iOS8-Safari");
 		driver.quit();
 	}
 	@Test(priority=5)
@@ -86,7 +86,7 @@ public class CheckoutProcess_TestMatrix {
 				+ "AppleWebKit/537.51.1(KHTML, like Gecko) Version/7.0 Mobile/11A4449d Safari/9537.53");
 		WebDriver driver= new FirefoxDriver(profile);
 		PaymentGatewayTest.Pg_Test(driver);	
-		System.out.println("Checkout working fine in iOS7-Safari");
+		System.out.println("6.Checkout working fine in iOS7-Safari");
 		driver.quit();
 	}
 	@Test(priority=6)
@@ -97,7 +97,7 @@ public class CheckoutProcess_TestMatrix {
 				+ " Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920");
 		WebDriver driver= new FirefoxDriver(profile);
 		PaymentGatewayTest.Pg_Test(driver);	
-		System.out.println("Checkout working fine in IE_Nokia_Lumia920 ");
+		System.out.println("7.Checkout working fine in IE_Nokia_Lumia920 ");
 		driver.quit();		
 	}
 	@Test(priority=7)
@@ -109,7 +109,7 @@ public class CheckoutProcess_TestMatrix {
 				+ "Presto/2.8.119 Version/11.10");
 		WebDriver driver= new FirefoxDriver(profile);
 		PaymentGatewayTest.Pg_Test(driver);
-		System.out.println("Checkout working fine in OperaMini_iPhone");
+		System.out.println("8.Checkout working fine in OperaMini_iPhone");
 		driver.quit();		
 	}
 	@Test(priority=8)
@@ -121,7 +121,7 @@ public class CheckoutProcess_TestMatrix {
         		+ " Presto/2.8.119 Version/11.1010");
 		WebDriver driver= new FirefoxDriver(profile);
 		PaymentGatewayTest.Pg_Test(driver);
-		System.out.println("Checkout working fine in OperaMini_Android");
+		System.out.println("9.Checkout working fine in OperaMini_Android");
 		driver.quit();		
 	}
 	@AfterClass
