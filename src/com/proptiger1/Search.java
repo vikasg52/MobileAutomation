684ab662d47cd867bb7bd4937cc43da141249289
vikas <vikas@vikas-Vostro-2420.(none)>
2015-01-19 03:21:42 +0530

    search test added

---
+++ src/com/proptiger1/Search.java
@@ -0,0 +1,110 @@
+package com.proptiger1;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+public class Search {
+	static String BaseUrl="https://www.proptiger.com";
+	static TimeStamp t= new TimeStamp();
+	 @SuppressWarnings("deprecation")
+	public static void search(WebDriver driver, String name) throws InterruptedException
+	 {
+		 driver.manage().window().setSize(new Dimension(300,630));
+		 driver.manage().deleteAllCookies();		
+		 driver.get(BaseUrl);
+		Thread.sleep(7000L);
+		driver.findElement(By.xpath("//a[@href='bangalore-real-estate-overview']")).click();
+		Thread.sleep(4000L);
+		boolean search_box= t.isElementPresent(driver , By.xpath("//input[@class='fake-search-box search-input wd85percent']"));
+		boolean search_button= t.isElementPresent(driver , By.xpath("//button[@class='srch-btn wd14percent']"));
+		String Default_Text= driver.findElement(By.xpath("//input[@class='fake-search-box search-input wd85percent']")).getAttribute("placeholder");
+        boolean Search_icon = t.isElementPresent(driver, By.xpath("//i[@class='icon-search']"));
+		if(search_box==false)
+        {
+        	Assert.fail("*.Search text box is missing on the city home page when accessed via "+name);
+        }
+        if(search_button==false)
+        {
+        	Assert.fail("*.Search button is missing on the city home page when accessed via "+name);	
+        }
+        if(Search_icon==false)
+        {
+        	Assert.fail("*.Search icon is missing on the city home page when accessed via "+name);	
+        }
+        if(!Default_Text.equalsIgnoreCase("Search for a project, builder or locality"))
+        {
+        	Assert.fail("*. Default text in the serach text box is not present or incorrect when accessed via "+name);	
+        }
+        driver.findElement(By.xpath("//input[@class='fake-search-box search-input wd85percent']")).click();
+        driver.getCurrentUrl();
+        if(!driver.getCurrentUrl().equalsIgnoreCase(BaseUrl+"/bangalore-real-estate-overview#searchPopup"))
+        {
+        	Assert.fail("*. After tapping on search text box search page is not opening when accessed via "+name);
+        }
+        //=============================
+        // search page object verification
+        //=============================
+        boolean search_pagebox= t.isElementPresent(driver, By.xpath("//input[@class='search-input wd65percent']"));
+        boolean Search_pageicon = t.isElementPresent(driver, By.xpath("//i[@class='icon-search']"));
+        boolean blue_text = t.isElementPresent(driver,By.xpath("//div[@class='ta-center blue']"));
+        boolean grey_text =t.isElementPresent(driver,By.xpath("//div[@class='ta-center grey]"));
+        if(search_pagebox==false && Search_pageicon==false)
+        {
+        	Assert.fail("*.Search box and search icon is missing on search page when accessed in "+name);
+        }
+        if(blue_text==false && grey_text==false)
+        {
+        	Assert.fail("*.Blue text and grey color text is missing from the search page");
+        }
+    	String bluetext=driver.findElement(By.xpath("//div[@class='ta-center blue']")).getText();
+    	if(!bluetext.equalsIgnoreCase("Start Exploring"))
+    	{
+    		Assert.fail("Blue text on search page is incorrect");
+    	}
+        String greytext=driver.findElement(By.xpath("//div[@class='ta-center grey']")).getText();
+        if(!greytext.equalsIgnoreCase("Search for a locality, project or builder"))
+    	{
+    		Assert.fail("Grey text on search page is incorrect");
+    	}
+        driver.findElement(By.xpath("//a[@class='no-ajaxy srch-cancel wd20percent']")).click();
+        if(!driver.getCurrentUrl().equalsIgnoreCase("https://www.proptiger.com/bangalore-real-estate-overview"))
+        {
+        	Assert.fail("*.After clicking cancel link on search page home page is not opening in "+name);
+        }
+        //=============================================
+        //Search result verification
+        //=============================================
+        driver.findElement(By.xpath("//input[@class='fake-search-box search-input wd85percent']")).click();
+        driver.findElement(By.xpath("//input[@class='search-input wd65percent']")).sendKeys("dl");
+        boolean searchResult= t.isElementPresent(driver, By.xpath("//div[@class='search-results']"));
+        boolean searchCancel=t.isElementPresent(driver,By.xpath("//span[@class='search-cancel']"));
+        boolean FirstResulticon= t.isElementPresent(driver, By.xpath("//div[@data-opt-idx='0']//i[@class='icon-apartment pull-left paddingR5']"));
+        String FirstResult=driver.findElement(By.xpath("//div[@class='put-ellipsis' and text()='DLF - Bella Greens - Bangalore']")).getText();
+        int Resultcount = driver.findElements(By.xpath("//div[@class='ac-opt']")).size();
+        if(searchResult==false && searchCancel==false)
+        {
+        	Assert.fail("search Results are not appearing/cancel button is missing after entering text 'dl' in text box on "+name);
+        }
+        if(Resultcount!=4)
+        {
+        	Assert.fail("Result Count is incorrect tested on -"+name);
+        }
+        if(FirstResulticon==false)
+        {
+        	Assert.fail("Wrong search result is displayed, After entering 'dl', First result icon not coming as 'Apartment' on"+name);
+        }
+        if(!FirstResult.equalsIgnoreCase("DLF - Bella Greens - Bangalore"))
+        {
+        	Assert.fail("First Result is displayed incorrectly: on"+name);
+        }
+        
+        // Click on search result verification
+        driver.findElement(By.xpath("//div[@class='put-ellipsis' and text()='DLF - Bella Greens - Bangalore']")).click();
+        Thread.sleep(3000L);
+        if(!driver.getCurrentUrl().equalsIgnoreCase("https://www.proptiger.com/bangalore/begur/dlf-bella-greens-513482"))
+        {
+        	Assert.fail("Clicking on first search result the next page is not opening/ url of next page is wrong-tested on"+name);
+        }
+	 }
+
+}
---
+++ src/com/proptiger1/Search_Testing.java
@@ -0,0 +1,143 @@
+package com.proptiger1;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterClass;
+public class Search_Testing {
+	
+	TimeStamp t= new TimeStamp();	
+	@BeforeTest
+	public void start()
+	{
+		System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.Jdk14Logger");
+		System.out.println("============== SEARCH TEST STARTED===============");
+	}	
+	@Test(priority=0)
+	void SearchChrome_iPhone() throws InterruptedException 
+	{
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setPreference("general.useragent.override", "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0_2 like Mac OS X) AppleWebKit/537.51.1 "
+				+ "(KHTML, like Gecko) CriOS/30.0.1599.12 Mobile/11A501 Safari/8536.25");
+		WebDriver driver= new FirefoxDriver(profile);
+		
+		String name="Chrome_Iphone";
+		Search.search(driver, name);
+		System.out.println("1.Search working fine in Chrome-iPhone");
+		driver.quit();
+	}
+	
+	@Test(priority=1)
+	void SearchChrome_AndroidTablet() throws InterruptedException 
+	{
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setPreference("general.useragent.override","Mozilla/5.0 (Linux; Android 4.1.2; Nexus 7 Build/JZ054K) AppleWebKit/535.19 "
+				+ "(KHTML, like Gecko) Chrome/18.0.1025.166 Safari/535.19");
+		WebDriver driver= new FirefoxDriver(profile);
+		
+		String name="Chrome_AndroidTablet";
+		Search.search(driver, name);
+		System.out.println("2.Search working fine in Chrome-Android-Tablet");
+		driver.quit();
+	}
+	
+	@Test(priority=2)
+	void SearchChrome_AndroidMobile() throws InterruptedException 
+	{
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setPreference("general.useragent.override","Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 "
+				+ "(KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19");
+		WebDriver driver= new FirefoxDriver(profile);
+		String name="Chrome_AndroidMobile";
+	    Search.search(driver, name);	
+		System.out.println("3.Search working fine in Chrome-AndroidMobile");
+		driver.quit();
+	}
+	@Test(priority=3)
+	void SearchFireFox_Android() throws InterruptedException 
+	{
+		TimeStamp t= new TimeStamp();;
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setPreference("general.useragent.override","Mozilla/5.0 (Android; Tablet; rv:14.0) Gecko/14.0 Firefox/14.0");
+		WebDriver driver= new FirefoxDriver(profile);		
+		String name= "FireFox-Android";
+		driver.manage().deleteAllCookies();
+	    Search.search(driver, name);	
+		System.out.println("4.Search working fine in FireFox-Android");
+		driver.quit();
+	}
+	@Test(priority=4)
+	void SearchSafari_iOS8() throws InterruptedException {
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setPreference("general.useragent.override","Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 "
+				+ "(KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4");
+		WebDriver driver= new FirefoxDriver(profile);
+		
+		String name="iOS8-Safari";
+		driver.manage().deleteAllCookies();
+	    Search.search(driver, name);	
+		System.out.println("5.Search working fine in iOS8-Safari");
+		driver.quit();
+	}
+	@Test(priority=5)
+	void SearchSafari_iOS7() throws InterruptedException 
+	{
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setPreference("general.useragent.override","Mozilla/5.0 (iPhone; CPU iPhone OS 7_0_2 like Mac OS X) "
+				+ "AppleWebKit/537.51.1(KHTML, like Gecko) Version/7.0 Mobile/11A4449d Safari/9537.53");
+		WebDriver driver= new FirefoxDriver(profile);
+				
+		String name= "iOS-Safari";
+		driver.manage().deleteAllCookies();
+	    Search.search(driver, name);	
+		System.out.println("6.Search working fine in iOS7-Safari");
+		driver.quit();
+	}
+	@Test(priority=6)
+	void SearchIE_Nokia_Lumia920() throws InterruptedException 
+	{
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setPreference("general.useragent.override","Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0;"
+				+ " Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920");
+		WebDriver driver= new FirefoxDriver(profile);
+				
+		String name="IE_Nokia_Lumia920";
+	    Search.search(driver, name);	
+		System.out.println("7.Search working fine in IE_Nokia_Lumia920 ");
+		driver.quit();		
+	}
+	@Test(priority=7)
+	void SearchOperaMini_iPhone() throws InterruptedException 
+	{
+		String name="OperaMini_iPhone";
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setPreference("general.useragent.override","Mozilla/5.0 (iPhone; CPU iPhone OS 8_1_2 like Mac OS X) "
+				+ "AppleWebKit/600.1.4 (KHTML, like Gecko) OPiOS/9.1.0.86723 Mobile/12B440 Safari/9537.53");
+		WebDriver driver= new FirefoxDriver(profile);
+				
+		Search.search(driver, name);
+		System.out.println("8.Search working fine in OperaMini_iPhone");
+		driver.quit();		
+	}
+	@Test(priority=8)
+	void SearchOperaMini_Android() throws InterruptedException 
+	{
+		String name="OperaMini_Android";
+		 FirefoxProfile profile = new FirefoxProfile();
+        profile.setPreference("general.useragent.override","Opera/9.80 (Android; Opera Mini/7.5.33361/31.1448; U; en)"
+        		+ " Presto/2.8.119 Version/11.1010");
+		WebDriver driver= new FirefoxDriver(profile);
+				
+		Search.search(driver, name);
+		System.out.println("9.Search working fine in OperaMini_Android");
+		driver.quit();		
+	}
+	@AfterClass
+	public static void teardown()  
+	{
+		System.out.println("============== SEARCH TEST FINISHED ==============\n");
+     }
+}
