5471054df399481571b01fc8cd50004d167d4815
vikasg52 <vikasg52@users.noreply.github.com>
2015-01-31 19:14:18 +0530

    Update MobileTestBed.java

--- src/com/proptiger1/MobileTestBed.java
+++ src/com/proptiger1/MobileTestBed.java
@@ -23,7 +23,7 @@ public class MobileTestBed {
 		profile.setPreference("general.useragent.override", "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0_2 like Mac OS X) AppleWebKit/537.51.1 "
 				+ "(KHTML, like Gecko) CriOS/30.0.1599.12 Mobile/11A501 Safari/8536.25");
 		WebDriver driver= new FirefoxDriver(profile);
-		driver.manage().deleteAllCookies();
+		//driver.manage().deleteAllCookies();
 	        driver.manage().window().setSize(new Dimension(540,630));
 		Cross_PlatForm.AllPages(driver,name);
 		//driver.quit();
@@ -44,7 +44,7 @@ public class MobileTestBed {
 				+ "(KHTML, like Gecko) Chrome/18.0.1025.166 Safari/535.19");
 		WebDriver driver= new FirefoxDriver(profile);
 		driver.manage().window().setSize(new Dimension(540,630));
-	driver.manage().deleteAllCookies();
+	        //driver.manage().deleteAllCookies();
 		Cross_PlatForm.AllPages(driver,name);
 		Cross_PlatForm.Check404Page(driver);
 		Cross_PlatForm.CheckAmenityPages(driver);
@@ -60,9 +60,9 @@ public class MobileTestBed {
 		profile.setPreference("general.useragent.override","Mozilla/5.0 (Linux; Android 4.4.4; XT1030 Build/SU4.21) "
 				+ "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.102 Mobile Safari/537.36");
 		WebDriver driver= new FirefoxDriver(profile);
-		driver.manage().deleteAllCookies();
+	//	driver.manage().deleteAllCookies();
 		driver.manage().window().setSize(new Dimension(540,630));
-	Cross_PlatForm.AllPages(driver,name);
+	        Cross_PlatForm.AllPages(driver,name);
 		Cross_PlatForm.Check404Page(driver);
 		Cross_PlatForm.CheckAmenityPages(driver);
 		System.out.println("7.All Pages are accessible on Chrome_AndroidMobile");
@@ -76,7 +76,7 @@ public class MobileTestBed {
 		profile.setPreference("general.useragent.override","Mozilla/5.0 (Android; Tablet; rv:14.0) Gecko/14.0 Firefox/14.0");
 		WebDriver driver= new FirefoxDriver(profile);
 		driver.manage().window().setSize(new Dimension(540,630));
-	driver.manage().deleteAllCookies();
+	       //driver.manage().deleteAllCookies();
 		Cross_PlatForm.AllPages(driver,name);
 		Cross_PlatForm.Check404Page(driver);
 		Cross_PlatForm.CheckAmenityPages(driver);
@@ -91,7 +91,7 @@ public class MobileTestBed {
 				+ "(KHTML, like Gecko) Version/8.0 Mobile/12B440 Safari/600.1.4");
 		WebDriver driver= new FirefoxDriver(profile);
 		driver.manage().window().setSize(new Dimension(540,630));
-	driver.manage().deleteAllCookies();
+	        //driver.manage().deleteAllCookies();
 		Cross_PlatForm.AllPages(driver,name);
 		Cross_PlatForm.Check404Page(driver);
 		Cross_PlatForm.CheckAmenityPages(driver);
@@ -106,9 +106,9 @@ public class MobileTestBed {
 		profile.setPreference("general.useragent.override","Mozilla/5.0 (iPhone; CPU iPhone OS 7_0_2 like Mac OS X) "
 				+ "AppleWebKit/537.51.1(KHTML, like Gecko) Version/7.0 Mobile/11A4449d Safari/9537.53");
 		WebDriver driver= new FirefoxDriver(profile);
-		driver.manage().deleteAllCookies();
+		//driver.manage().deleteAllCookies();
 		driver.manage().window().setSize(new Dimension(540,630));
-	Cross_PlatForm.AllPages(driver,name);
+	       Cross_PlatForm.AllPages(driver,name);
 		Cross_PlatForm.Check404Page(driver);
 		Cross_PlatForm.CheckAmenityPages(driver);
 		System.out.println("8.All Pages are accessible on iOS7");
@@ -123,7 +123,7 @@ public class MobileTestBed {
 				+ " Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920");
 		WebDriver driver= new FirefoxDriver(profile);
 		driver.manage().window().setSize(new Dimension(540,630));
-	driver.manage().deleteAllCookies();
+	        driver.manage().deleteAllCookies();
 		Cross_PlatForm.AllPages(driver,name);	
 		Cross_PlatForm.Check404Page(driver);
 		Cross_PlatForm.CheckAmenityPages(driver);
@@ -139,7 +139,7 @@ public class MobileTestBed {
 				+ "AppleWebKit/600.1.4 (KHTML, like Gecko) OPiOS/9.1.0.86723 Mobile/12B440 Safari/9537.53");
 		WebDriver driver= new FirefoxDriver(profile);
 		driver.manage().window().setSize(new Dimension(540,630));
-	Cross_PlatForm.AllPages(driver,name);	
+	        Cross_PlatForm.AllPages(driver,name);	
 		Cross_PlatForm.Check404Page(driver);
 		Cross_PlatForm.CheckAmenityPages(driver);
 		System.out.println("2.All Pages are accessible on OperaMini_iPhone");
@@ -153,9 +153,9 @@ public class MobileTestBed {
         profile.setPreference("general.useragent.override","Opera/9.80 (Android; Opera Mini/7.5.33361/31.1448; U; en)"
         		+ " Presto/2.8.119 Version/11.1010");
 		WebDriver driver= new FirefoxDriver(profile);
-		driver.manage().deleteAllCookies();
-		driver.manage().window().setSize(new Dimension(540,630));
-	Cross_PlatForm.AllPages(driver,name);	
+	//	driver.manage().deleteAllCookies();
+	    	driver.manage().window().setSize(new Dimension(540,630));
+	        Cross_PlatForm.AllPages(driver,name);	
 		Cross_PlatForm.Check404Page(driver);
 		Cross_PlatForm.CheckAmenityPages(driver);
 		System.out.println("4.All Pages are accessible on OperaMini_Android");
