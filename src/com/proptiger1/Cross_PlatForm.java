48bd497543af28a0c8b1ba25d02758297b9737be
vikasg52 <vikasg52@users.noreply.github.com>
2015-02-03 02:28:59 +0530

    Update Cross_PlatForm.java

--- src/com/proptiger1/Cross_PlatForm.java
+++ src/com/proptiger1/Cross_PlatForm.java
@@ -87,7 +87,7 @@ driver.manage().window().setSize(new Dimension(540,650));
 				driver.navigate().back();
 				Thread.sleep(3000L);
 			}
-			driver.findElement(By.xpath("//img[@src='https://im.proptiger.com/1/643769/6/samruddhi-group-wintergreen-elevation-555334.jpeg?width=400&height=300']")).click();
+			driver.findElement(By.xpath("//img[contains(@src,'https://im.proptiger.com/1/643769/6/samruddhi')]")).click();
 			t1.wait(driver, "//h1[@class='proj-name put-ellipsis']");
 			String ProjectPage= driver.getCurrentUrl();
 			String Projectheading= driver.findElement(By.xpath("//h1[@class='proj-name put-ellipsis']")).getText();
