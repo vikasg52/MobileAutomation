a47bcc780405ac2e819ffc0675508ff996cfd667
vikasg52 <vikasg52@users.noreply.github.com>
2015-04-01 18:41:48 +0530

    Create Mailer_Number_Check.java

---
+++ src/com/proptiger1/Mailer_Number_Check.java
@@ -0,0 +1,35 @@
+package com.proptiger1;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.testng.annotations.Test;
+
+public class Mailer_Number_Check {
+	static String name="";
+	static String http="http://www.proptiger.com";
+	static String ssl="https://www.proptiger.com";
+	static String betahttp="http://beta.proptiger-ws.com";
+	static String mobbeta="http://mob-beta.proptiger-ws.com";
+	static String local= "http://192.168.0.216:5000";
+	static String betassl="https://beta.proptiger-ws.com";
+	static String BaseUrl=betassl;
+	@Test()
+	public void numbertest() throws InterruptedException
+	{
+		Phone_Number_Check Check_Number= new Phone_Number_Check();
+		Sem_ListPage checkNumber= new Sem_ListPage();
+	        //System.out.println("You have entered : " + BaseUrl);
+        checkNumber.Sem_ListingPage(BaseUrl);
+        Check_Number.GooglePages_SEM_Number(BaseUrl);
+		System.out.println("\n");
+		//driver.manage().deleteAllCookies();
+		Check_Number.internal_Mailer(BaseUrl);
+		//driver.manage().deleteAllCookies();
+		System.out.println("\n");
+		Check_Number.External_Mailer(BaseUrl);
+		}
+	public void teardown()
+	{
+		
+	}
+}
