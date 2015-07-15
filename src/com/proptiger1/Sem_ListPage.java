38cd61a54d45fe5648b73370916d9d6641144ba7
Vikas Garg <vikasg52@users.noreply.github.com>
2015-07-15 17:11:47 +0530

    Update Sem_ListPage.java

--- src/com/proptiger1/Sem_ListPage.java
+++ src/com/proptiger1/Sem_ListPage.java
@@ -18,11 +18,12 @@ public class Sem_ListPage<t> {
 	TimeStamp t= new TimeStamp();
 	 @SuppressWarnings("deprecation")
 	@Test(priority=0)
-	public <t> void Sem_ListingPage(String x) throws InterruptedException
+	public void Sem_ListingPage(String x) throws InterruptedException
 	{
-		String prod="https://www.proptiger.com";
-		String beta="https://qa.proptiger-ws.com";
-		String BaseUrl11=beta;
+	String beta="https://beta.proptiger-ws.com";
+	String prod="https://www.proptiger.com";
+	String qa="https://qa.proptiger-ws.com";
+	String BaseUrl11=beta;
 		FirefoxProfile profile = new FirefoxProfile();
 		profile.setPreference("general.useragent.override", "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0_2 like Mac OS X) AppleWebKit/537.51.1 "
 				+ "(KHTML, like Gecko) CriOS/30.0.1599.12 Mobile/11A501 Safari/8536.25");
@@ -37,6 +38,7 @@ public class Sem_ListPage<t> {
 			{   "Mumbai",
 				"Noida",
 				"Gurgaon",
+				"Banglore",
 				"Pune",
 				"Chennai",
 				"Ghaziabad",
@@ -45,6 +47,7 @@ public class Sem_ListPage<t> {
 			   {"+912261054545",
 				"+911166765333",
 				"+911166765111",
+				"+918033512373",
 				"+912067082636",
 				"+914439942555",
 			    "+919555606060"};
@@ -53,7 +56,7 @@ public class Sem_ListPage<t> {
 				BaseUrl11+"/projects-in-mumbai"+Cookie,
 				BaseUrl11+"/projects-in-noida"+Cookie,
 				BaseUrl11+"/projects-in-gurgaon"+Cookie,
-				//BaseUrl11+"/projects-in-bangalore"+Cookie,
+				BaseUrl11+"/projects-in-bangalore"+Cookie,
 				BaseUrl11+"/projects-in-pune"+Cookie,
 				BaseUrl11+"/projects-in-chennai"+Cookie,
 				BaseUrl11+"/projects-in-ghaziabad"+Cookie};
